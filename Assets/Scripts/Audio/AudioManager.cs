60ceed20b1e919354bd065670c97821113777ae4
Carlos Marques <catralitos11@gmail.com>
2023-03-11 15:54:02 +0000

    Adicionei código para audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public Sound[] sounds;
+
+        private Sound _intro;
+        private Sound _loop;
+
+        /*
+        #region SingleTon
+        public static AudioManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            // Needed if we want the audio manager to persist through scenes
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            // Add audio source components
+            foreach (Sound s in sounds)
+            {
+                s.SetSource(gameObject.AddComponent<AudioSource>());
+            }
+        }
+        
+
+        #endregion
+        */
+
+        private void Awake()
+        {
+            foreach (Sound s in sounds)
+            {
+                s.SetSource(gameObject.AddComponent<AudioSource>());
+            }        
+        }
+
+        public void Play(string soundName)
+        {
+            var s = Array.Find(sounds, sound => sound.name == soundName);
+            if (s == null)
+            {
+                Debug.LogWarning("Sound " + soundName + " not found!");
+                return;
+            }
+
+            if (s.IsPlaying()) return;
+            s.Play();
+        }
+
+        public void Stop(string soundName)
+        {
+            var s = Array.Find(sounds, sound => sound.name == soundName);
+            if (s == null)
+            {
+                Debug.LogWarning("Sound " + soundName + " not found!");
+                return;
+            }
+            if (!s.IsPlaying()) return;
+            s.Stop();
+        }
+
+        public void SetMusic(string introName, string loopName)
+        {
+            if (_intro != null && introName == _intro.name && _loop != null && loopName == _loop.name)
+            {
+                Debug.Log(introName + "/" + loopName + " already playing, ignoring.");
+                return;
+            }
+
+            if (_intro != null)
+                _intro.Stop();
+            if (_loop != null)
+                _loop.Stop();
+
+            _intro = Array.Find(sounds, sound => sound.name == introName);
+            if (_intro == null)
+            {
+                Debug.LogWarning("Sound " + introName + " not found!");
+                return;
+            }
+
+            _loop = Array.Find(sounds, sound => sound.name == loopName);
+            if (_intro == null)
+            {
+                Debug.LogWarning("Sound " + loopName + " not found!");
+                return;
+            }
+
+            var introDuration = _intro.clip.length;
+            var startTime = AudioSettings.dspTime + 0.2;
+            _intro.PlayScheduled(startTime);
+            _loop.PlayScheduled(startTime + introDuration);
+        }
+
+        public bool IsPlaying(String soundName)
+        {
+            var s = Array.Find(sounds, sound => sound.name == soundName);
+            if (s != null) return s.IsPlaying();
+            Debug.LogWarning("Sound " + soundName + " not found!");
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+
+        [Range(0f, 1f)]
+        public float volume = 1f;
+        [Range(0.1f, 3f)]
+        public float pitch = 1f;
+        public bool loop;
+
+        [HideInInspector]
+        public AudioSource source;
+
+        public void SetSource(AudioSource audioSource)
+        {
+            audioSource.clip = clip;
+
+            audioSource.volume = volume;
+            audioSource.pitch = pitch;
+            audioSource.loop = loop;
+            this.source = audioSource;
+        }
+
+        public void Play()
+        {
+            source.Play();
+        }
+
+        public void PlayScheduled(double time)
+        {
+            source.PlayScheduled(time);
+        }
+
+        public void Stop()
+        {
+            source.Stop();
+        }
+
+        public bool IsPlaying()
+        {
+            return source.isPlaying;
+        }
+    }
+}
